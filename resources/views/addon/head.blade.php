a16e751fa232e2cd27765540a3a01d3ad0368d0d
Rizqy Candra Saputra <rizqycndra@gmail.com>
2023-11-14 21:19:08 +0700

    fix: tailwind config

--- resources/views/addon/head.blade.php
+++ resources/views/addon/head.blade.php
@@ -3,5 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ !is_null($title ?? null) ? $title . " | Hoop - Ecommerce" : "Hoop - Ecommerce" }}</title>
-    @vite("resource/css/app.css")
+    @vite('resources/css/app.css')
 </head>
