933e4b092747499f6e96f7c51d65864a211be34a
Rizqy Candra Saputra <rizqycndra@gmail.com>
2023-12-31 14:21:44 +0700

    build: make banner model & simplification seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Banner;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Product::factory(10)->create();
+        Category::factory(4)->create();
+        for ($i = 1; $i <= 10; $i++) { 
+            Product::find($i)->categories()->attach([rand(0, 1), rand(2, 3)]);
+        }
+        Review::factory(20)->create();
+        for ($i = 1; $i <= 3; $i++) { 
+            Banner::factory(1)->create();
+        }
     }
 }
